f01ff554fee8f8fc68cf9e1616492620de894440
MirzaabdullayevTest <mirzaabdullayevtest@mail.ru>
2021-08-24 19:22:42 +0500

    ES6 first lesson

---
+++ index.js
@@ -0,0 +1,118 @@
+/*
+number
+string
+boolean
+null
+undefined
+array
+object
+*/
+
+/*
+var
+*/
+
+/*
+if else
+switch case
+for
+function
+*/
+
+/*
+&&
+||
+!=
+*/
+
+// =======================> ES6
+// let const 
+// for (let i = 0; i <= 9; i++) {
+//     setTimeout(function () {
+//         console.log(i);
+//     }, 2000);
+// }
+
+// var color = 'red'
+// color = 'green'
+
+// let color = 'black'
+// color = 'white'
+// console.log(color);
+
+// ====================> const 
+// const id = 'AB4545'
+// // id = 'AB45454877777777'
+// console.log(id);
+
+// const arr = [1, 2]
+// const obj = { a: 2 }
+// arr.push(10)
+// obj.a = 10
+// console.log(arr);
+// console.log(obj);
+
+// ------------------------- function // arrow funtions
+// function calc(a, b) {
+//     return a + b
+// }
+
+// let calc = function (a, b) {
+//     return a + b
+// }
+
+// let func = (a, b) => {
+//     return a + b
+// }
+
+// let func = (a, b) => a + b
+// console.log(func(5, 4));
+
+// let func = a => a
+// console.log(func(5));
+
+// ====================> object 
+// let person = {
+//     name: 'Tom',
+//     age: 50,
+//     job: {
+//         who: 'Dev',
+//         salary: 45000
+//     }
+// }
+
+// let name = person.name
+// let age = person.age
+// let age = person.age
+
+// let { age, name, job: { salary } } = person
+// console.log(salary);
+
+//  =================================== array
+
+// let arr = ['Tom', 25, { who: 'dev', salary: 45000 }]
+
+// let [name, age, { who: value }] = arr
+
+// console.log(value);
+
+// =================================> assetsatsivniy massivlar
+
+// let arr = [
+//     { name: 'Tom', age: 50 },
+//     { name: 'Karl', age: 30 },
+//     { name: 'Yuri', age: 15 },
+// ]
+
+// // arr.forEach(element => {
+// //     console.log(element.age);
+// // });
+
+// let [{ name: val }] = arr
+
+// val = 'Ricki'
+
+// console.log(val);
+
+const arr = ['Kim', 50]
+console.log(arr);
